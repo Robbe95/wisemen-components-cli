e8ba79a4ea4404f558c133cb723767d86f52d1ae
Robbe Vaes <robbevaes95@gmail.com>
2023-07-16 20:55:24 +0200

    Changed back to static url

--- src/utils/staticVariables.ts
+++ src/utils/staticVariables.ts
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.COMPONENTS_BASE_URL ?? "https://deploy-preview-4--wisemen-components.netlify.app"
+export const baseUrl = process.env.COMPONENTS_BASE_URL ?? "https://wisemen-components.netlify.app"
